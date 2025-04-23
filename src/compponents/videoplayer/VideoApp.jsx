c7a8eb32067ebaa321eb77639aae75222ef513a1
mehedi hasan <mhshohan04@gmail.com>
2025-04-23 16:51:30 +0600

    Specify the Effect dependencies

--- src/compponents/videoplayer/VideoApp.jsx
+++ src/compponents/videoplayer/VideoApp.jsx
@@ -4,9 +4,17 @@ import VideoPlayer from "./VideoPlayer";
 
 export default function VideoApp() {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [text, setText] = useState('');
+
   return (
     <>
+      <input
+          className="border-black bg-amber-600 rounded-lg mr-2 "
+          placeholder="type here"
+          value={text}
+          onChange={e => setText(e.target.value)} />
       <button
+          className="bg-gray-500 text-black-500 rounded-md px-2 py-1"
           onClick={() =>
           setIsPlaying(!isPlaying)}>
         {isPlaying ? 'Pause' : 'Play'}
