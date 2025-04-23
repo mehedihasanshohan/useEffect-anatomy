f4267ce93ed5d61f1fd5369e01fdbcf9765175c3
mehedi hasan <mhshohan04@gmail.com>
2025-04-23 15:58:50 +0600

    projects setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+    <h2 className='text-center'><span className='text-rose-600 font-bold'><i className='text-orange-400 font-bold'>UseEffect anatomy</i></span></h2>
+   </>
+  )
+}
+
+export default App
