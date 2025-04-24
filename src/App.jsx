24b337d3e7ae43a82fe462ab4481a1f2be405321
mehedi hasan <mhshohan04@gmail.com>
2025-04-24 12:00:45 +0600

    Implemented cleanup function to observe unmount behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
-import Timer from './compponents/cleanup/Timer'
-import MyComponents from './compponents/MyComponents'
-import VideoApp from './compponents/videoplayer/VideoApp'
+import Effecting from './compponents/cleanup/Effecting'
+ import VideoApp from './compponents/videoplayer/VideoApp'
 
 function App() {
 
   return (
    <>
     <h2 className='text-center'><span className='text-rose-600 font-bold'><i >UseEffect </i></span><span className='text-orange-400 font-bold'>anatomy</span></h2>
-   <MyComponents></MyComponents>
-    <VideoApp></VideoApp>
-    <Timer></Timer>
+   {/* <MyComponents></MyComponents> */}
+    {/* <VideoApp></VideoApp> */}
+    <Effecting></Effecting>
    </>
   )
 }
---
+++ src/compponents/cleanup/Effecting.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+
+const Effecting = () => {
+  const [click, setClick] = useState(0);
+
+  useEffect(()=>{
+    console.log("effecting");
+    // cleanup function
+    return ()=>{
+        console.log('cleanup')
+    }
+  });
+
+
+  return (
+    <div>Effecting
+    <button
+        className='bg-blue-900 text-white px-2 rounded-sm ml-2'
+        onClick={()=> setClick(click+1)}>clicked: {click}</button>
+    </div>
+  )
+}
+
+export default Effecting
